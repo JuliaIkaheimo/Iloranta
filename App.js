da85251ec23f1289797d478dbddcb72aa046c162
JenniLehtonen <59826954+JenniLehtonen@users.noreply.github.com>
2021-11-08 13:59:43 +0200

    Create some initial pages

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+//Import the screens
+import MainScreen from './screens/mainScreen';
+import MapScreen from './screens/mapScreen';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Etusivu">
+
+        <Drawer.Screen name="Etusivu" options={{ headerShown: false }}>
+          {props => <MainScreen {...props} />}
+        </Drawer.Screen>
+
+        <Drawer.Screen name="Kartta" options={{ headerShown: false }}>
+          {props => <MapScreen {...props} />}
+        </Drawer.Screen>
+
+      </Drawer.Navigator>
+    </NavigationContainer>
+    <StatusBar style="auto" />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/mainScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import styles from '../styles/mainScreenStyle';
+
+export default function MainScreen() {
+    return(
+        <View style={styles.container}>
+            <Text>Main page</Text>
+        </View>
+    );
+}
