b7090986009b83b59d21a032fc4b1cc07640aa30
JenniLehtonen <59826954+JenniLehtonen@users.noreply.github.com>
2021-11-08 14:20:57 +0200

    Some changes

--- screens/mainScreen.js
+++ screens/mainScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import styles from '../styles/mainScreenStyle';
 
 export default function MainScreen() {
     return(
         <View style={styles.container}>
+            <Image source={require('../assets/logo.png')}/>
             <Text>Main page</Text>
         </View>
     );
