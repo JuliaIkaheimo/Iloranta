3e0e22d26d27ad84e299f17fa325b94f5639a400
Jenna Aaltonen <jenna19105@student.hamk.fi>
2021-11-14 17:08:35 +0200

    front page styling

--- styles/mainScreenStyle.js
+++ styles/mainScreenStyle.js
@@ -2,9 +2,48 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
     container: {
+      flex: 1,
+      flexDirection:'column',
       height: '100%',
       justifyContent: "center",
-      padding: 20,
-      backgroundColor: "white"
-    }
+      alignItems:'center',
+      backgroundColor: "white",
+      
+    },
+
+     logo:{
+       flex: 1,
+       paddingBottom:10
+      },
+
+     textContainer:{
+       flex:2,
+       width:'80%',
+       justifyContent: 'space-around',
+       alignItems:'flex-start',
+       padding: 40,
+       paddingBottom: 50,
+       
+     },
+
+     text:{
+      textAlignVertical: "center",
+       textAlign: "center",
+       fontSize: 16,
+       color: 'black'
+     },
+
+     languageContainer:{
+      flex:1,
+      width:'85%',
+      flexDirection:'row',
+      justifyContent:'space-between',
+     },
+
+     languageImage:{
+       width:50,
+       height:50,
+     }
+
+
   });
