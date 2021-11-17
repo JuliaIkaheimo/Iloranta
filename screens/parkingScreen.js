3d574ef4bdaf3760b9f4c2f24805ae7dde02e09a
JenniLehtonen <59826954+JenniLehtonen@users.noreply.github.com>
2021-11-17 12:05:24 +0200

    Change the data and continue building the page

--- screens/parkingScreen.js
+++ screens/parkingScreen.js
@@ -1,34 +1,48 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, Image} from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 import styles from '../styles/parkingScreenStyle';
 import MenuButton from '../components/menuButton';
+
+//Information about the different accommodation places
 import accommodation from '../data/accommodation.json';
 
 
 export default function ParkingScreen() {
-    const [selectedAccommodation, setSelectedAccommodation] = useState();
+    const [selectedAccommodation, setSelectedAccommodation] = useState("Päärakennus");
+    const [parking, setParking] = useState("Voit jättää autosi Punaisen talon taakse tai kesällä Sylvia Reginan alapuolelle.");
+
+    useEffect(() => {
+       let findParkingLot = accommodation.find(place => place.title == selectedAccommodation);
+       setParking(findParkingLot.parkingLot);
+       console.log(parking);
+      },[selectedAccommodation]);
 
     return(
         <>
             <MenuButton />
             <View style={styles.container}>
-            <Image style={styles.image} resizeMode="contain" source={require('../assets/parking.png')}/>
-                <Text style={styles.h1}>Etsi tästä parkkipaikka:</Text>
+                <Image style={styles.image} resizeMode="contain" source={require('../assets/parking.png')}/>
+                <Text style={styles.h1}>Valitse majoituspaikkasi:</Text>
                 <View style={styles.pickerContainer}>
-                <Picker style={styles.pickerText}
-                    selectedValue={selectedAccommodation}
-                    onValueChange={(itemValue, itemIndex) =>
-                        setSelectedAccommodation(itemValue)
-                    }>
-                        { 
-                        accommodation.map(item => 
-                            <Picker.Item key={item.title} label={item.title} value={item.title} />
-                        )
-                    }
-                        
-                </Picker>
+                    <Picker style={styles.pickerText}
+                        selectedValue={selectedAccommodation}
+                        onValueChange={(itemValue, itemIndex) =>
+                            setSelectedAccommodation(itemValue)
+                        }>
+                            { 
+                                accommodation.map(item => 
+                                    <Picker.Item key={item.title} label={item.title} value={item.title} />
+                                )
+                            }
+                            
+                    </Picker>
                 </View>
+                <View style={styles.parkingLotContainer}>
+                    <Text style={styles.h2}>Sinulle sopiva parkkipaikka on:</Text>
+                    <Text style={styles.text}>{parking}</Text>
+                </View>
+                
                 
 
             </View>
--- styles/parkingScreenStyle.js
+++ styles/parkingScreenStyle.js
@@ -10,6 +10,13 @@ export default StyleSheet.create({
     pickerContainer: {
         width: "70%"
     },
+    parkingLotContainer: {
+        backgroundColor:"lightblue",
+        width: "100%",
+        padding: 20,
+        alignItems: "center"
+    },
+
     //Texts
     h1: {
         fontSize: 25,
@@ -18,9 +25,20 @@ export default StyleSheet.create({
         marginTop: 45,
         marginBottom: 25,
     },
+    h2: {
+        fontSize: 20,
+        color: "#2F2F2F",
+        fontWeight: "bold",
+        textAlign: "center"
+    },
     pickerText: {
         color: "#2F2F2F"
     },
+    text: {
+        fontSize: 16,
+        color: "#2F2F2F"
+    },
+    
     //For the icon on top of the page
     image:{
         height: "18%"
