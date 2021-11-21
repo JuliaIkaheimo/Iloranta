47c82ffbd7f4097ada022bcde849bd884ca2aa60
Kirsi Suoranta <suorantakirsi@gmail.com>
2021-11-21 18:27:15 +0200

    Installed Picker
    Installed Picker to select category in MapView.

--- styles/mapScreenStyle.js
+++ styles/mapScreenStyle.js
@@ -2,8 +2,9 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
     container: {
-      height: '88%',
-      justifyContent: "center"
+      height: '100%',
+      alignItems: 'center',
+      backgroundColor: "white"
     },
     mapStyle: {
         width: '100%',
@@ -11,6 +12,25 @@ export default StyleSheet.create({
     },
     titleText: {
       fontSize: 18,
-    }
-
+    },
+    calloutBubble: {
+      flex: 1,
+      position: 'relative',
+      padding: 5,
+    },
+    pickerContainer: {
+      width: "100%",
+      height: "5%",
+      justifyContent:"center",
+      borderColor: "#2F2F2F",
+      borderWidth: 1,
+    },
+    pickerText: {
+      color: "#2F2F2F",
+    },
+    mapContainer: {
+      width: "100%",
+      height: "82%",
+      alignItems: "center"
+  },
  });
